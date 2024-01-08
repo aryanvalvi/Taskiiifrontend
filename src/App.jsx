4f0fefcd80e175a53141fc85fbf713e150bde710
Aryan <aryanvalvi323@gmail.com>
2024-01-08 09:02:50 +0530

    all files added

---
+++ src/App.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Update from "./Update";
+import { Link } from "react-router-dom";
+const App = () => {
+  const [getAll, setGetAll] = useState([]);
+  const [pushTask, SetPushTask] = useState("");
+  const [workdone, SetWorkdon] = useState(false);
+  const [rerender, SetRerender] = useState(false);
+  console.log(workdone);
+  const GetAll = () => {
+    axios
+      .get("http://localhost:5000/api/todo/")
+      .then((res) => {
+        setGetAll(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    GetAll();
+  }, [rerender]);
+
+  const AddTask = (edata) => {
+    const ronaldo = {
+      message: edata,
+      completed: workdone,
+    };
+    axios
+      .post("http://localhost:5000/api/todo/Create/", ronaldo)
+      .then((res) => {
+        console.log("data bhej diya", res);
+        SetRerender(!rerender);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const TaskDelete = (e) => {
+    const Id = {};
+    axios
+      .delete(`http://localhost:5000/api/todo/delete/${e}`)
+      .then((res) => {
+        console.log("Deleted");
+        SetRerender(!rerender);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const ChnageButtonvalue = () => {
+    SetWorkdon(!workdone);
+  };
+
+  return (
+    <div className="Container">
+      <h1 className="h1">TASKIII</h1>
+      <div className="chota-container">
+        <input
+          className="search"
+          type="text"
+          onChange={(e) => SetPushTask(e.target.value)}
+        />
+        <p>Completed</p>
+        <input
+          className="check"
+          // value={workdone}
+          checked={workdone}
+          onChange={ChnageButtonvalue}
+          type="checkbox"
+        />
+      </div>
+      <button className="Addtask" onClick={() => AddTask(pushTask)}>
+        Add Task
+      </button>
+
+      {/* <button onClick={GetAll}>GetAll</button> */}
+
+      <div className="allget">
+        {getAll.map((e) => {
+          return (
+            <div key={e._id} className="task-items">
+              <p>{e.message}</p>
+
+              <div className="items">
+                <input className="box" type="checkbox" checked={e.completed} />
+
+                <button className="delete" onClick={() => TaskDelete(e._id)}>
+                  Delete
+                </button>
+                <Link to={`/Update/${e._id}`}>
+                  <button className="update"> Update</button>
+                </Link>
+              </div>
+              {/* {isVisible && <Update value={e._id}></Update>} */}
+              {/* <button onClick={() => TaskUpdate(e._id)}>Update</button> */}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Update.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+const Update = () => {
+  const { idParam } = useParams();
+  const [Thatone, SetThatone] = useState([]);
+  console.log(Thatone);
+  const [updatedMessage, setUpdatedMessage] = useState();
+  // const istrueornot = Thatone[0].completed;
+  const [alreadytik, setAlreadytik] = useState();
+  const [workdone, SetWorkdon] = useState(false);
+  console.log(workdone);
+
+  useEffect(() => {
+    button();
+    // setAlreadytik(Thatone[0].completed);
+    // console.log(Thatone);
+  }, []);
+
+  const button = async () => {
+    try {
+      const res = await axios.get(`http://localhost:5000/api/todo/${idParam}`);
+      SetThatone([res.data]);
+
+      SetWorkdon(res.data.completed);
+      console.log(res.data.message);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const UpdateValue = (data) => {
+    const fuck = {
+      message: data,
+      completed: workdone,
+    };
+    axios.patch(`http://localhost:5000/api/todo/update/${idParam}`, fuck);
+  };
+  const ChnageButtonvalue = () => {
+    SetWorkdon(!workdone);
+  };
+
+  return (
+    <div className="Container">
+      {/* <button onClick={() => button}>btn</button> */}
+      {/* {Thatone} */}
+      {Thatone.map((e) => {
+        return (
+          <div className="updatecontainer">
+            <div className="updatechota">
+              <h1>Message:-</h1>
+              <p>{e.message}</p>
+            </div>
+            <h1>
+              Completed
+              <input
+                className="chec"
+                // value={workdone}
+                checked={workdone}
+                onChange={ChnageButtonvalue}
+                type="checkbox"
+              />
+            </h1>
+
+            <input
+              className="input"
+              type="text"
+              value={updatedMessage}
+              onChange={(e) => setUpdatedMessage(e.target.value)}
+            />
+            <button
+              className="Addtask"
+              onClick={() => UpdateValue(updatedMessage)}
+            >
+              Update
+            </button>
+          </div>
+        );
+      })}
+      <Link className="bth" to="/">
+        Back to Home
+      </Link>
+    </div>
+  );
+};
+
+export default Update;
